c80ed0f100aeddd6653f4480fc1c862e17ba7c87
Alex Caica <al.caica@duocuc.cl>
2025-10-24 18:13:16 -0300

    ADD: HomeScreen base y NavController

--- app/src/main/java/com/example/levelupgamer_grupo10/MainActivity.kt
+++ app/src/main/java/com/example/levelupgamer_grupo10/MainActivity.kt
@@ -4,44 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.levelupgamer_grupo10.ui.theme.LevelUpGamer_Grupo10Theme
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.levelupgamer_grupo10.view.homeScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            LevelUpGamer_Grupo10Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            val navController = rememberNavController()
+            NavHost(navController = navController, startDestination = "HomeScreen") {
+                composable("HomeScreen") {
+                    homeScreen(navController)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LevelUpGamer_Grupo10Theme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/example/levelupgamer_grupo10/view/HomeScreen.kt
@@ -0,0 +1,46 @@
+package com.example.levelupgamer_grupo10.view
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import com.example.levelupgamer_grupo10.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun homeScreen(navController: NavHostController) {
+    Scaffold(
+        topBar = {
+            TopAppBar(title = { Text("Tienda Level-Up Gamer") })
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(text = "Bienvenid@!",
+                fontWeight = FontWeight.Bold
+            )
+        }
+    }
+}
