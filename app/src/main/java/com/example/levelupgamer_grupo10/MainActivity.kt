c020ec92712f0e8ac8ba109fc7b884a174b02c4b
Alex Caica <al.caica@duocuc.cl>
2025-10-24 15:59:25 -0300

    Inicio proyecto + estructura base MVVM

---
+++ app/src/main/java/com/example/levelupgamer_grupo10/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.levelupgamer_grupo10
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.levelupgamer_grupo10.ui.theme.LevelUpGamer_Grupo10Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LevelUpGamer_Grupo10Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    LevelUpGamer_Grupo10Theme {
+        Greeting("Android")
+    }
+}
